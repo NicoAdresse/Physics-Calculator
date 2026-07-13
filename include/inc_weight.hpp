b55d69b42681b27d1c747ec3ff17694102e42f29
Nico <nicobsc4@yahoo.com>
2026-07-13 16:23:29 +0200

    Added include/inc_weight.hpp

---
+++ include/inc_weight.hpp
@@ -0,0 +1,14 @@
+// include/inc_weight.hpp
+
+#pragma once
+
+/*
+Formulas (Weight):
+    weight = mass * gravity (w = m * g)
+*/
+
+class WeightFormulas
+{
+public:
+    void calculateWeight(const double mass, const double gravity);
+};
