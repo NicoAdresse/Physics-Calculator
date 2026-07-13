bd5b0f5270e3b0b03ea0160c8bcb691f9b8a0c71
Nico <nicobsc4@yahoo.com>
2026-07-13 16:27:54 +0200

    Added impl/impl_weight.cpp

---
+++ impl/impl_weight.cpp
@@ -0,0 +1,19 @@
+// impl/impl_weight.cpp
+
+#include "helper/helper_funcs.hpp"
+#include "../include/inc_weight.hpp"
+
+void WeightFormulas::calculateWeight(const double mass, const double gravity)
+{
+    double result{mass * gravity};
+
+    displayResult(
+        "Weight",
+        mass,
+        gravity,
+        result,
+        "Mass | m",
+        "Gravity | g",
+        '*'
+    );
+}
